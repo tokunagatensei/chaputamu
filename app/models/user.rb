13f17fb61c20ebc371fce67d1645c9d28f14b6b8
tokunagatensei <tensei.soccer14@icloud.com>
2023-03-02 00:55:55 +0900

    ビューの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :last_name,         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :first_name,        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :birth_date
+  end
+
+  validates :password,            format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+
 end
