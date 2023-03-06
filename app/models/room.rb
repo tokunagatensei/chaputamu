6aa947f4ed46419f1f2f962f1322065bfc842e21
tokunagatensei <tensei.soccer14@icloud.com>
2023-03-06 15:00:59 +0900

    ルーム作成機能を実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,11 @@
 class Room < ApplicationRecord
+
+  with_options presence: true do
+    validates :room_name
+    validates :group_name
+    validates :introduction
+  end
+
   has_many :room_users
   has_many :users, through: :room_users
 end
