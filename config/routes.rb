3b3e5ae215ed4aa9710a151a50e25095b050a7c5
tokunagatensei <tensei.soccer14@icloud.com>
2023-03-10 15:35:23 +0900

    メッセージ送信・表示機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'messages/index'
   devise_for :users
-root to: "rooms#index"
-resources :users, only: [:show, :edit, :update]
-resources :rooms, only: [:index, :new, :create]
+  root to: "rooms#index"
+  resources :users, only: [:show, :edit, :update]
+  resources :rooms, only: [:index, :new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
